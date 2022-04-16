561343446f335ae681ad4017a6adf8e66ebebc4b
fzTelU <fazaalexander@student.telkomuniversity.ac.id>
2022-04-16 22:51:17 +0700

    menambahkan class runtime configuration

---
+++ Tubes1KPL_Kelompok7/LaundryConfig.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Tubes1KPL_Kelompok7
+{
+    public class Config
+    {
+        public LaundryConfig conf;
+        public string filePath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        public string fileConfigName = "laundry_config.json";
+        public Config()
+        {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch
+            {
+                SetDefault();
+                WriteNewConfigFile();
+            }
+        }
+        private LaundryConfig ReadConfigFile()
+        {
+            String jsonString = File.ReadAllText(filePath + @"\" + fileConfigName);
+            conf = JsonSerializer.Deserialize<LaundryConfig>(jsonString);
+            return conf;
+        }
+
+        private void SetDefault()
+        {
+            conf = new LaundryConfig("english", "Dolar");
+        }
+
+        private void WriteNewConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            string jsonString = JsonSerializer.Serialize(conf, options);
+            string fullFilePath = filePath + @"\" + fileConfigName;
+            File.WriteAllText(fullFilePath, jsonString);
+        }
+    }
+
+    public class LaundryConfig
+    {
+        public string bahasa { get; set; }
+        public string mataUang { get; set; }
+
+        public LaundryConfig() { }
+
+        public LaundryConfig(string bahasa, string mataUang)
+        {
+            this.bahasa = bahasa;
+            this.mataUang = mataUang;
+        }
+    }
+
+}
--- Tubes1KPL_Kelompok7/Program.cs
+++ Tubes1KPL_Kelompok7/Program.cs
@@ -6,7 +6,19 @@ namespace Tubes1KPL_Kelompok7
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Config Laundry = new Config();
+            Console.WriteLine("Language: ");
+            string bahasa = Console.ReadLine();
+            if (bahasa == "english" || bahasa == "en")
+            {
+                Console.WriteLine("Currency: ");
+                string mataUang = Console.ReadLine();
+            }
+            else if (bahasa == "indonesia" || bahasa == "id")
+            {
+                Console.WriteLine("Mata Uang: ");
+                string mataUang = Console.ReadLine();
+            }
         }
     }
 }
